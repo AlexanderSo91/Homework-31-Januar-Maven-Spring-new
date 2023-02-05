4fa47ed7b8f7946d9d09ff7178fb76ca52d6092b
zotin <zotinalexander@gmail.com>
2023-02-05 14:26:58 +0100

    commit

---
+++ src/main/java/com/example/homework31januarmavenspringnew/Homework31JanuarMavenSpringNewApplication.java
@@ -0,0 +1,13 @@
+package com.example.homework31januarmavenspringnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework31JanuarMavenSpringNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework31JanuarMavenSpringNewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homework31januarmavenspringnew/Homework31JanuarMavenSpringNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework31januarmavenspringnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework31JanuarMavenSpringNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
